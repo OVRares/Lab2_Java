a818e94d30d4f585fa02ba7889653c60f9726828
OVRares <rares.olariu-vanasila@student.upt.ro>
2024-02-29 09:42:39 +0200

    Commit 3

---
+++ src/ex2/MainApp.java
@@ -0,0 +1,7 @@
+package ex2;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ex2/Vers.java
@@ -0,0 +1,19 @@
+package ex2;
+
+public class Vers {
+    private String linie;
+    Vers (String l)
+    {
+        linie = l;
+    }
+
+    public int getLenght()
+    {
+       return linie.length();
+
+    }
+
+
+
+
+}
