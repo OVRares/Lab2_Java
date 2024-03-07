bf60a5a2abc389bde7ae742d57c790aaa4f7145d
Rares Olariu-Vanasila <OVRares>
2024-03-07 11:34:11 +0200

    Commit 2

--- src/ex2/MainApp.java
+++ src/ex2/MainApp.java
@@ -1,7 +1,41 @@
 package ex2;
 
+import java.io.*;
+
 public class MainApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String nume_fis = "src/ex2/cantec_in.txt";
+        BufferedReader flux_in;
+        flux_in = new BufferedReader(new FileReader(nume_fis));
+        int line_nr = 0;
+        String line;
+        line = flux_in.readLine();
+        while (line != null) {
+            line_nr++;
+            line = flux_in.readLine();
+        }
+        System.out.println(line_nr);
+        flux_in.close();
+
+        Vers[] strofa;
+        strofa = new Vers[line_nr];
+        flux_in = new BufferedReader(new FileReader(nume_fis));
+        PrintStream flux_out;
+        flux_out = new PrintStream("src/ex2/cantec_out.txt");
+
+        int j;
+        line = flux_in.readLine();
+        for (j = 0; j < line_nr; j++) {
+            strofa[j] = new Vers(line);
+            line = flux_in.readLine();
+            j++;
+
+        }
+        flux_in.close();
+
+        for (j = 0; j < line_nr; j++)
+            System.out.println(strofa[j]);
+
 
     }
 }
--- src/ex2/Vers.java
+++ src/ex2/Vers.java
@@ -1,15 +1,33 @@
 package ex2;
 
 public class Vers {
-    private String linie;
+    private String v_linie;
     Vers (String l)
     {
-        linie = l;
+        v_linie = l;
     }
 
-    public int getLenght()
+    public int getWordCount()
     {
-       return linie.length();
+       String[] s = v_linie.split(" ");
+       return s.length;
+    }
+
+    public int getVowelCount()
+    {
+        int count = 0;
+        for (int i=0;i<v_linie.length(); i++)
+            if (v_linie.charAt(i) == 'a' || v_linie.charAt(i) == 'i' ||
+                    v_linie.charAt(i) == 'u' || v_linie.charAt(i) == 'e' || v_linie.charAt(i) == 'o')
+                count++;
+        else
+            if (v_linie.charAt(i) == 'A' || v_linie.charAt(i) == 'I' ||
+                    v_linie.charAt(i) == 'U' || v_linie.charAt(i) == 'E' || v_linie.charAt(i) == 'O')
+                count++;
+        return count;
+
+
+
 
     }
 
